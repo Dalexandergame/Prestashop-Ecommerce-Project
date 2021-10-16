2886c7103402ac5230d9975f422f7e735948b0b3
Kadiri <el.abdelhafid@pulse.digital>
2021-10-16 19:03:56 +0100

    stripe

--- themes/ecosapin/assets/js/custom.js
+++ themes/ecosapin/assets/js/custom.js
@@ -2,3 +2,13 @@
  * Custom code goes here.
  * A template should always ship with an empty custom.js
  */
+
+$(function() {
+    if ($("body.page-order").length) {
+        setTimeout(function () {
+            $([document.documentElement, document.body]).animate({
+                scrollTop: $("section.-current").offset().top
+            }, 1000);
+        }, 2000)
+    }
+})
