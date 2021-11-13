381ea33af6aa89148235206006fa87fd86006d8e
Kadiri <el.abdelhafid@pulse.digital>
2021-11-13 17:41:45 +0100

    Revert "test"
    This reverts commit caf86b6ca769b82678e16fba3f08da8ef1116db9.

--- modules/planningdeliverybycarrier/js/functions.js
+++ modules/planningdeliverybycarrier/js/functions.js
@@ -34,7 +34,7 @@ function getDaySlotForm()
 		return (document.day_slot_form);
 }
 
-function getPlanningDaySlot(path, id_day, format, id_lang, onAdminPlanningDelivery, id_carrier)
+function getDaySlot(path, id_day, format, id_lang, onAdminPlanningDelivery, id_carrier)
 {
 	$.get(path, {id_day: id_day, format: format, id_lang: id_lang, onAdminPlanningDelivery: onAdminPlanningDelivery, id_carrier: id_carrier, ajax: 1, no_cache: $.now() },
 	function(data){
