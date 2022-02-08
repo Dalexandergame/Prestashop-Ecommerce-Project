c58dde611dbf458617e617599ff539895009ed37
Brahim Bakkas <b.brahim@pulse.digital>
2022-02-08 10:03:43 +0100

    added the sms for each store for each language

--- _application/resources/lang/en/delivery.php
+++ _application/resources/lang/en/delivery.php
@@ -48,17 +48,17 @@ return [
 
     //sms messages
         //Ecosapin CH
-        "sms1_1" => "Hello,\nYour Christmas tree will be delivered tomorrow, between 7.30 and 19.00. You’ll receive a confirmation text 30 to 90 minutes before the delivery.",
-        "sms2_1" => "Hello,\nI have 3 Christmas trees to deliver before yours, I’m on my way ! If you’re not home, I’ll drop it in front of your door. \nYour Christmas tree delivery man",
-        "sms3_1" => "We will pick up your Christmas tree tomorrow between 7.30 and 19.00. Please let your tree in front of your door, where our deliveryman can access easily.",
+        "sms1_1" => "Hello,\nYour Christmas tree will be delivered tomorrow, between 7.30 and 19.00. You’ll be informed by text 30 to 90 minutes before the delivery.\nEcosapin",
+        "sms2_1" => "Hello,\nI have 3 Christmas trees to deliver before yours, I’m on my way ! If you’re not home, I’ll drop it in front of your door. \nEcosapin",
+        "sms3_1" => "We will pick up your Christmas tree tomorrow between 7.30 and 19.00. Please let your tree in front of your door, where our deliveryman can access it.\nEcosapin",
         //Ecosapin FR
-        "sms1_2" => "\nYour Christmas tree will be delivered tomorrow, between 7.30 and 19.00. You’ll receive a confirmation text 30 to 90 minutes before the delivery.",
-        "sms2_2" => "\nI have 3 Christmas trees to deliver before yours, I’m on my way ! If you’re not home, I’ll drop it in front of your door. \nYour Christmas tree delivery man",
-        "sms3_2" => "l pick up your Christmas tree tomorrow between 7.30 and 19.00. Please let your tree in front of your door, where our deliveryman can access easily.",
+        "sms1_2" => "Bonjour,\nVotre sapin vous sera livré demain, entre 7h30 et 19h. Un SMS de confirmation vous parviendra entre 30 et 90 minutes avant la livraison.\nVotre sapiniste",
+        "sms2_2" => "Bonjour,\nIl reste 3 sapins à livrer avant le votre, je suis en route. En cas d'absence, je le déposerai devant votre porte.\nVotre sapiniste",
+        "sms3_2" => "Notre livreur viendra récupérer votre sapin demain (7h30 - 19h). Nous vous invitons à laisser votre sapin à un endroit accessible.\nVotre sapiniste",
         //My Abies
-        "sms1_3" => "delivered tomorrow, between 7.30 and 19.00. You’ll receive a confirmation text 30 to 90 minutes before the delivery.",
-        "sms2_3" => "deliver before yours, I’m on my way ! If you’re not home, I’ll drop it in front of your door. \nYour Christmas tree delivery man",
-        "sms3_3" => "tomorrow between 7.30 and 19.00. Please let your tree in front of your door, where our deliveryman can access easily.",
+        "sms1_3" => "Hello,\nYour Christmas tree will be delivered tomorrow, between 7.30 and 19.00. You’ll be informed by text 30 to 90 minutes before the delivery.\nMyAbies",
+        "sms2_3" => "Hello,\nI have 3 Christmas trees to deliver before yours, I’m on my way ! If you’re not home, I’ll drop it in front of your door. \nMyAbies",
+        "sms3_3" => "We will pick up your Christmas tree tomorrow between 7.30 and 19.00. Please let your tree in front of your door, where our deliveryman can access it.\nMyAbies",
 
     "sendFirstSMS" => "Send first three SMS",
     "sendTomorrowSMS" => "Send tomorrow's SMS",
